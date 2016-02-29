b87d37f0f2e2ac076c282bbf540749c56da2358d
ntente3 <ntente4@gmail.com>
2016-02-29 20:47:24 +0900

    기본기능 구현

---
+++ EraseFileList/EraseFileList/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace EraseFileList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "실행";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(522, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(633, 160);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(12, 205);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(633, 184);
+            this.listBox2.Sorted = true;
+            this.listBox2.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 395);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(633, 35);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 442);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ EraseFileList/EraseFileList/Form1.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EraseFileList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.Minimum = 0;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"C:\ProgramData\AVAST Software\Avast\GrimeFighter2";
+            ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string openFileName = ofd.FileName;
+                textBox2.Text = openFileName;
+
+                string[] filelines = File.ReadAllLines(openFileName);
+                progressBar1.Maximum = filelines.Length;
+
+                foreach (string filename in filelines)
+                {
+                    // 파일
+                    if (filename.IndexOf("HKEY") != 0)
+                    {
+//                        if (filename.IndexOf(@"C:\WINDOWS") == 0)
+                            //continue;
+                        FileInfo fi = new FileInfo(filename);
+                        try
+                        {
+                            if (fi.Exists == false)
+                            {
+                                listBox1.Items.Add("Already Deleted File Name : " + filename);
+                                listBox1.Refresh();
+                                progressBar1.PerformStep();
+                                continue;
+                            }
+                            fi.Delete();
+                            listBox1.Items.Add("Deleted File Name : " + filename);
+                            listBox1.Refresh();
+                            progressBar1.PerformStep();
+                        }
+                        catch (Exception ex)
+                        {
+                            listBox2.Items.Add(ex.ToString());
+                            listBox2.Refresh();
+                            progressBar1.PerformStep();
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        // 레지스트리
+                        try
+                        {
+                            string reginame = null;
+                            if (filename.IndexOf(@"HKEY_USERS\") == 0)
+                            {
+                                reginame = filename.Remove(0, 11);
+                                RegistryKey reg = Registry.Users.OpenSubKey(reginame);
+                                if (reg != null)
+                                {
+                                    Registry.Users.DeleteSubKey(reginame);
+                                    progressBar1.PerformStep();
+                                }
+                                else
+                                {
+                                    listBox2.Items.Add("No exist Registry Key : " + filename);
+                                    listBox2.Refresh();
+                                    progressBar1.PerformStep();
+                                    continue;
+                                }
+                            }
+                            else if (filename.IndexOf(@"HKEY_CURRENT_USER\") == 0)
+                            {
+                                reginame = filename.Remove(0, 18);
+                                RegistryKey reg = Registry.CurrentUser.OpenSubKey(reginame);
+                                if (reg != null)
+                                {
+                                    Registry.CurrentUser.DeleteSubKey(reginame);
+                                    progressBar1.PerformStep();
+                                }
+                                else
+                                {
+                                    listBox2.Items.Add("No exist Registry Key : " + filename);
+                                    listBox2.Refresh();
+                                    progressBar1.PerformStep();
+                                    continue;
+                                }
+                            }
+                            else if (filename.IndexOf(@"HKEY_LOCAL_MACHINE\") == 0)
+                            {
+                                reginame = filename.Remove(0, 19);
+                                RegistryKey reg = Registry.LocalMachine.OpenSubKey(reginame);
+                                if (reg != null)
+                                {
+                                    Registry.LocalMachine.DeleteSubKey(reginame);
+                                    progressBar1.PerformStep();
+                                }
+                                else
+                                {
+                                    listBox2.Items.Add("No exist Registry Key : " + filename);
+                                    listBox2.Refresh();
+                                    progressBar1.PerformStep();
+                                    continue;
+                                }
+                            }
+                            listBox1.Items.Add("Remove Registry Key : " + filename);
+                            listBox1.Refresh();
+                            progressBar1.PerformStep();
+                        }
+                        catch(Exception ex)
+                        {
+                            listBox2.Items.Add(ex.ToString());
+                            listBox2.Refresh();
+                            progressBar1.PerformStep();
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
